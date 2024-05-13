e7f3089e8c0aa1d059e06fa8f23ef3a61d3e6e20
MyCr3ation <www.work.mustafa@gmail.com>
2024-05-13 10:20:36 +0400

    Removed 2 warnings

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import profile from "../../assets/person.jpeg";
 import { MdOutlineMenu } from "react-icons/md";
 import "./Navbar.css";
 import { useLocation, useNavigate } from "react-router-dom";
